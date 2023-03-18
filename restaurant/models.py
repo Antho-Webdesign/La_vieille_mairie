34d9df169f158f0ff532592b521034113604bf16
AnthoWebDesign <toutchy.kipik@outlook.com>
2023-03-19 00:24:28 +0100

    restaurant app migarion

--- restaurant/models.py
+++ restaurant/models.py
@@ -1,3 +1,35 @@
 from django.db import models
+from django.utils.text import slugify
 
-# Create your models here.
+TVA_1 = 0.1  # TVA de 10%
+TVA_2 = 0.2  # TVA de 20%
+
+
+class CategorieMenu(models.Model):
+    nom = models.CharField(max_length=100)
+    img_cat = models.ImageField(upload_to='images/categories', null=True, blank=True)
+    slug = models.SlugField(unique=True, editable=False)
+
+    def save(self, *args, **kwargs):
+        self.slug = slugify(self.nom)
+        super(CategorieMenu, self).save(*args, **kwargs)
+
+    def __str__(self):
+        return self.nom
+
+
+class ElementMenu(models.Model):
+    nom = models.CharField(max_length=100)
+    description = models.TextField()
+    img_item = models.ImageField(upload_to='images/items', null=True, blank=True)
+    prix = models.DecimalField(max_digits=5, decimal_places=2)
+    categorie = models.ForeignKey(CategorieMenu, on_delete=models.CASCADE)
+    slug = models.SlugField(unique=True, editable=False)
+    tva_choice = models.CharField(max_length=5, default=TVA_1, choices=[(TVA_1, 'TVA 10%'), (TVA_2, 'TVA 20%')])
+
+    def save(self, *args, **kwargs):
+        self.slug = slugify(self.nom)
+        super(ElementMenu, self).save(*args, **kwargs)
+
+    def __str__(self):
+        return self.nom

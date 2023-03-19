95d49d3349f859537df0a7c396ffd202bcb0b93f
AnthoWebDesign <toutchy.kipik@outlook.com>
2023-03-19 03:23:18 +0100

    base design

--- restaurant/views.py
+++ restaurant/views.py
@@ -1,4 +1,20 @@
 from django.shortcuts import render
+from django.views.generic import ListView
+
+from restaurant.models import CategorieMenu
+
+
+class Header(ListView):
+    model = CategorieMenu
+    template_engine = 'header'
+
+
+def base(request):
+    categorie = CategorieMenu.objects.all()
+    context = {
+        'categories': categorie,
+    }
+    return render(request, 'includes/header.html', context)
 
 
 # Create your views here.
